4621b2d59d70ffea4e6a12e5c865193028726825
Kaj-Ivar van der Wijst <k.i.vanderwijst@gmail.com>
2024-02-23 07:34:16 +0100

    Package info

--- setup.py
+++ setup.py
@@ -1,15 +1,22 @@
 from setuptools import setup
 
+
+with open("README.md") as fh:
+    description = fh.read()
+
+with open("LICENSE") as fh:
+    license_txt = fh.read()
+
 setup(
     name="ar6_utils",
     version="0.1.0",
     author="Kaj-Ivar van der Wijst",
     author_email="k.i.vanderwijst@gmail.com",
     packages=["ar6_utils"],
-    # url="http://pypi.python.org/pypi/PackageName/",
-    license="LICENSE.txt",
+    url="https://kvanderwijst.github.io/ar6_utils/",
+    license=license_txt,
     description="Useful for handling AR6 database files in IAMC format, and provides AR6 plot style",
-    long_description=open("README.md").read(),
-    install_requires=["pandas", "openpyxl"],
+    long_description=description,
+    install_requires=["numpy", "pandas", "plotly", "openpyxl"],
     include_package_data=True,
 )
